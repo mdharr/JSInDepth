54c38049e6b14b4ec72293f585a8200cac11dc2f
Michael Harrington <mdharr7@gmail.com>
2023-12-09 11:18:35 +0800

    Complete exercises on variables and primitive types

---
+++ exercises/variables-and-primitive-types/script.js
@@ -0,0 +1,80 @@
+/* 
+   Create a constant variable of each of the following primitive types:
+   - String
+   - Number
+   - Boolean
+   - Null
+   - Undefined
+
+   Use console.log to log out each variable to the terminal at the end.
+
+   HINT1: You can run your file in the terminal using this command: node <file_name>
+          For example, if your file is called ps1.js, you would type: node ps1.js
+
+   HINT2: Make sure your terminal is "looking at" the correct folder where your code is
+*/
+
+
+// const myName = "Michael"
+// const favoriteNumber = 7
+// const hasChildren = true
+// const carModel = null
+// const carMake = undefined
+// console.log(myName)
+// console.log(favoriteNumber)
+// console.log(hasChildren)
+// console.log(carModel)
+// console.log(carMake)
+
+/* 
+    1. Create a variable called name that points at a string containing your name
+    2. On the next line, re-assign the variable above to your favourite musician's name
+    3. On the next line, re-assign the variable again to the value: true
+    3. On the next line, re-assign the variable one last time to the value: 42
+
+    After each of the above steps, log out the variable to the terminal with console.log.
+*/
+
+
+// let name = "Michael"
+// console.log(name)
+// name = "Damon Albarn"
+// console.log(name)
+// name = true
+// console.log(name)
+// name = 42
+// console.log(name)
+
+
+
+/* 
+    1. Declare a variable using let with any name you like, but without a value.
+    2. On a separate line, assign the variable the String: "Moo!"
+    3. Log out this variable to the terminal using console.log
+    
+    Repeat the above steps but try using a const variable. What happens?
+*/
+
+// let animalSound;
+// animalSound = "Moo!"
+// console.log(animalSound)
+
+// const birdSound;
+// const declarations must be initialized
+
+
+
+/* 
+    Use Google/search to look up the *typeof* operator in Javascript.
+
+    Use typeof to log out the types for each of the 5 primitive types
+    to the terminal using console.log.
+
+    *HINT* - MDN Web Docs are a good resource
+*/
+
+// console.log(typeof 'string')
+// console.log(typeof 42)
+// console.log(typeof true)
+// console.log(typeof null)
+// console.log(typeof undefined)
