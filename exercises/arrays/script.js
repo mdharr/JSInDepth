e5244378de9f4dfdef20bd51591af2189db38162
Michael Harrington <mdharr7@gmail.com>
2023-12-10 20:16:45 +0800

    Rebuilt JavaScripts built in Map function using Array.prototype

--- exercises/arrays/script.js
+++ exercises/arrays/script.js
@@ -164,12 +164,42 @@ let HTMLString =
 //     return charArray.join('')
 // }
 
-function closeDivTags(str) {
-    let counter = 0
-    return str.replace(/<div>/g, () => {
-        counter++
-        return (counter % 2 === 0) ? '</div>' : '<div>'
-    })
+// function closeDivTags(str) {
+//     let counter = 0
+//     return str.replace(/<div>/g, () => {
+//         counter++
+//         return (counter % 2 === 0) ? '</div>' : '<div>'
+//     })
+// }
+
+// console.log(closeDivTags(HTMLString))
+
+// using String.prototype to create isPalindrome function that has no parameters
+// String.prototype.isPalindrome = function () {
+//     let reversedString = this.split('').reverse().join('')
+//     if(String(this) === reversedString) {
+//         console.log(this.toString(), reversedString)
+//         return true
+//     } else {
+//         console.log(this.toString(), reversedString)
+//         return false
+//     }
+// }
+
+// let radar = 'radar'
+// console.log(radar.isPalindrome())
+
+// recreate Map function based on Array.prototype
+Array.prototype.myMap = function (cb) {
+    const newArray = []
+    for(let i = 0; i < this.length; i++) {
+        newArray[i] = cb(this[i])
+    }
+    return newArray
 }
 
-console.log(closeDivTags(HTMLString))
+const arr1 = [1, 2, 3].myMap(console.log)
+const arr2 = ["a", "b", "c"].myMap(console.log)
+
+console.log(arr1)
+console.log(arr2)
