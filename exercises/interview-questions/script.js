693ca453186ad560c6afd2ed09e455a271e5c084
Michael Harrington <mdharr7@gmail.com>
2023-12-20 10:59:40 +0800

    Write multiple solutions for checking if a user by specified name in an array of user objects exists.

--- exercises/interview-questions/script.js
+++ exercises/interview-questions/script.js
@@ -25,10 +25,79 @@
 //     .map(user => user.name)
 
 // console.log(names)
-const numbers = [0, 1, 2]
-function addElementToArray(arr, element) {
-    return [...arr, element]
+// const numbers = [0, 1, 2]
+// function addElementToArray(arr, element) {
+//     return [...arr, element]
+// }
+
+// console.log(addElementToArray([0, 1, 2], 3))
+// console.log(numbers)
+
+// const arr1 = [0, 1, 2]
+// const arr2 = [3, 4, 5]
+
+// function concatArrays(arr1, arr2) {
+//     // return arr1.concat(arr2)
+//     return [...arr1, ...arr2]
+// }
+
+// const result = concatArrays(arr1, arr2)
+// console.log(result)
+// console.log(arr1)
+// console.log(arr2)
+
+const users = [
+    {
+        id: 1,
+        name: "Jack",
+        isActive: true,
+        age: 20,
+    },
+    {
+        id: 2,
+        name: "John",
+        isActive: true,
+        age: 18,
+    },
+    {
+        id: 3,
+        name: "Mike",
+        isActive: false,
+        age: 30,
+    },
+]
+
+// low level solution
+// function checkNameExists(name, users) {
+//     let exists = false
+//     for (let i = 0; i < users.length; i++) {
+//         if(users[i].name === name) {
+//             exists = true
+//         }
+//     }
+//     return exists
+// }
+
+// console.log(checkNameExists('John', users))
+
+// best solution
+// function checkNameExists(name, arr) {
+//     return arr.some(user => user.name === name)
+// }
+
+// console.log(checkNameExists('John', users))
+
+// using find and wrapping element in Boolean()
+// function checkNameExists(name, arr) {
+//     const el = arr.find(el => el.name === name)
+//     return Boolean(el)
+// }
+
+// one more solution using findIndex
+// if the index doesn't exist, it will be < 0
+function checkNameExists(name, arr) {
+    const index = arr.findIndex(el => el.name === name)
+    return index > 0
 }
 
-console.log(addElementToArray([0, 1, 2], 3))
-console.log(numbers)
+console.log(checkNameExists('John', users))
