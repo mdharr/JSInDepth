532a1ab0445b18b1033e49a854bd604c07d943cd
Michael Harrington <mdharr7@gmail.com>
2023-12-20 10:42:44 +0800

    Append new element to an array and return as a new array (different reference from original)

--- exercises/interview-questions/script.js
+++ exercises/interview-questions/script.js
@@ -1,26 +1,34 @@
-const users = [
-    {
-        id: 1,
-        name: "Jack",
-        isActive: true,
-        age: 20,
-    },
-    {
-        id: 2,
-        name: "John",
-        isActive: true,
-        age: 18,
-    },
-    {
-        id: 3,
-        name: "Mike",
-        isActive: false,
-        age: 30,
-    },
-]
+// const users = [
+//     {
+//         id: 1,
+//         name: "Jack",
+//         isActive: true,
+//         age: 20,
+//     },
+//     {
+//         id: 2,
+//         name: "John",
+//         isActive: true,
+//         age: 18,
+//     },
+//     {
+//         id: 3,
+//         name: "Mike",
+//         isActive: false,
+//         age: 30,
+//     },
+// ]
 
-const names = users.filter(user => user.isActive).map(user => user.name)
+// const names = users
+//     .sort((a,b) => a.age - b.age)
+//     .filter(user => user.isActive)
+//     .map(user => user.name)
+
 // console.log(names)
+const numbers = [0, 1, 2]
+function addElementToArray(arr, element) {
+    return [...arr, element]
+}
 
-const sortByAge = users.sort((a,b) => a.age - b.age)
-console.log(sortByAge)
+console.log(addElementToArray([0, 1, 2], 3))
+console.log(numbers)
