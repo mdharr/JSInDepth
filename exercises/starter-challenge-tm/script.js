81a1e041e9d46136c84f5d9118392dbb6ff84bf1
Michael Harrington <mdharr7@gmail.com>
2023-12-21 14:22:09 +0800

    Write example of function with space and time complexity of O(1)

--- exercises/starter-challenge-tm/script.js
+++ exercises/starter-challenge-tm/script.js
@@ -773,25 +773,34 @@
 // console.log(sumArray(largeArray))
 // console.timeEnd('Large Array')
 
-function findPower(base, exponent) {
-    if(exponent === 0) {
-        return 1
-    }
+// function findPower(base, exponent) {
+//     if(exponent === 0) {
+//         return 1
+//     }
 
-    if(exponent % 2 === 0) {
-        const halfPower = findPower(base, exponent / 2)
-        return halfPower * halfPower
-    } else {
-        const halfPower = findPower(base, (exponent - 1) / 2)
-        return base * halfPower * halfPower
-    }
+//     if(exponent % 2 === 0) {
+//         const halfPower = findPower(base, exponent / 2)
+//         return halfPower * halfPower
+//     } else {
+//         const halfPower = findPower(base, (exponent - 1) / 2)
+//         return base * halfPower * halfPower
+//     }
+// }
+
+// console.time('Find Power 1')
+// console.log(findPower(2, 5))
+// console.timeEnd('Find Power 1')
+
+// console.time('Find Power 2')
+// console.log(findPower(2, 10))
+// console.timeEnd('Find Power 2')
+
+/*
+    Space Complexity: O(1)
+    Time Complexity: O(1)
+*/
+
+function add(num1, num2) {
+    return num1 + num2
 }
-
-console.time('Find Power 1')
-console.log(findPower(2, 5))
-console.timeEnd('Find Power 1')
-
-console.time('Find Power 2')
-console.log(findPower(2, 10))
-console.timeEnd('Find Power 2')
 
