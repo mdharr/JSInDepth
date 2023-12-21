d713d22e9b5c81b6617d320dfe6c8ca0242630fe
Michael Harrington <mdharr7@gmail.com>
2023-12-21 14:51:15 +0800

    Write example of function with space complexity of O(1) and a time complexity of O(n)

--- exercises/starter-challenge-tm/script.js
+++ exercises/starter-challenge-tm/script.js
@@ -855,3 +855,17 @@ function findPower(base, exponent) {
     }
 }
 
+/*
+    Space Complexity: O(1)
+    Time Complexity: O(n)
+*/
+
+function findSum(arr) {
+    let sum = 0
+
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i]
+    }
+    return sum
+}
+
