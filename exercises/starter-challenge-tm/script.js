845ed376c7b16b4cd190aeaf4a4caee2890893bf
Michael Harrington <mdharr7@gmail.com>
2023-12-21 14:45:15 +0800

    Write example of function with space and time complexity of O(n)

--- exercises/starter-challenge-tm/script.js
+++ exercises/starter-challenge-tm/script.js
@@ -804,3 +804,17 @@ function add(num1, num2) {
     return num1 + num2
 }
 
+/*
+    Space Complexity: O(n)
+    Time Complexity: O(n)
+*/
+
+function createArray(num) {
+    const arr = []
+
+    for (let i = 0; i < num; i++) {
+        arr.push(i)
+    }
+    return arr
+}
+
