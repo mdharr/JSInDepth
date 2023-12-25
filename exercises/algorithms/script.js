09287a951bed23b03e3953730ab62a81a567fe07
Michael Harrington <mdharr7@gmail.com>
2023-12-25 13:41:17 +0800

    Additional notes on space complexity

--- exercises/algorithms/script.js
+++ exercises/algorithms/script.js
@@ -1,4 +1,4 @@
-// Big-O Time complexity
+// Big-O Time complexity --------------------------
 function summation(n) {
     let sum = 0 // executed once
     for (let i = 1; i <= n; i++) {
@@ -38,3 +38,23 @@ for (i = 1; i <= n; i++) { // outmost loop
 // if the input size reduces by half every iteration
 // it is logarithmic
 // O(logn) O of log n
+
+
+
+// Space Complexity -------------------------------
+
+// if the algorithm does not need extra memory
+// or the memory needed does not depend on the input
+// size, the space complexity is `constant`
+
+// an example would be sorting algorithms which sort
+// within the array without using additional arrays
+
+
+// linear space complexity where the extra space 
+// needed grows as the input size grows
+
+
+// logarithmic space complexity is when the extra
+// space needed grows, but not at the same rate 
+// as the input size
