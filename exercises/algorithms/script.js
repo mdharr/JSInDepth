9c1756e39a589949145cd7b9a9fafe81a402fe67
Michael Harrington <mdharr7@gmail.com>
2023-12-26 21:04:41 +0800

    Write insertionSort algorithm in JavaScript

--- exercises/algorithms/script.js
+++ exercises/algorithms/script.js
@@ -512,22 +512,43 @@
 // Problem - Given an array of integers, sort the array.
 
 // my solution with do while loop
-function bubbleSort(arr) {
-    let swapped;
-    do {
-        swapped = false
-        for(let i = 0; i < arr.length-1; i++) {
-            if(arr[i] > arr[i+1]) {
-                let temp = arr[i]
-                arr[i] = arr[i+1]
-                arr[i+1] = temp
-                swapped = true
-            }
-        }
-    } while(swapped)
-    return arr
-}
+// function bubbleSort(arr) {
+//     let swapped;
+//     do {
+//         swapped = false
+//         for(let i = 0; i < arr.length-1; i++) {
+//             if(arr[i] > arr[i+1]) {
+//                 let temp = arr[i]
+//                 arr[i] = arr[i+1]
+//                 arr[i+1] = temp
+//                 swapped = true
+//             }
+//         }
+//     } while(swapped)
+//     return arr
+// }
 
-console.log(bubbleSort([-6, 20, 8, -2, 4]))
+// console.log(bubbleSort([-6, 20, 8, -2, 4]))
 // this bubble sort algorithm has a Big-O time complexity
 // of O(n^2) / quadratic time complexity
+
+
+// Insertion Sort
+// Problem - Given an array of integers, sort the array
+
+// my solution
+function insertionSort(arr) {
+    for(let i = 1; i < arr.length; i++) {
+        let numberToInsert = arr[i]
+        let j = i - 1
+        while(j >= 0 && arr[j] > numberToInsert) {
+            arr[j+1] = arr[j]
+            j = j - 1
+        }
+        arr[j+1] = numberToInsert
+    }
+}
+
+const arr = [8, 20, -2, 4, -6]
+insertionSort(arr)
+console.log(arr)
