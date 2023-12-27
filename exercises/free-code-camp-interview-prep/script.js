7924fe0211cd452ce7cedd16a742f70641dd403e
Michael Harrington <mdharr7@gmail.com>
2023-12-27 14:35:13 +0800

    Solution for updating current inventory with new inventory.

--- exercises/free-code-camp-interview-prep/script.js
+++ exercises/free-code-camp-interview-prep/script.js
@@ -1,18 +1,35 @@
-// Find the Symmetric Difference
-function sym(...arrays) {
-    let result = new Set()
-    for(const currentArray of arrays) {
-        const currentSet = new Set(currentArray)
-        for(const elem of currentSet) {
-            if(result.has(elem)) {
-                result.delete(elem)
-            } else {
-                result.add(elem)
-            }
-        }
-    }
-    return Array.from(result)
-}
+// Find the Symmetric Difference -----------------------------------------------
+// function sym(...arrays) {
+//     let result = new Set()
+//     for(const currentArray of arrays) {
+//         const currentSet = new Set(currentArray)
+//         for(const elem of currentSet) {
+//             if(result.has(elem)) {
+//                 result.delete(elem)
+//             } else {
+//                 result.add(elem)
+//             }
+//         }
+//     }
+//     return Array.from(result)
+// }
 
-console.log(sym([1, 2, 5], [2, 3, 5], [3, 4, 5]))
-console.log(sym([1, 2, 3, 3], [5, 2, 1, 4]))
+// console.log(sym([1, 2, 5], [2, 3, 5], [3, 4, 5]))
+// console.log(sym([1, 2, 3, 3], [5, 2, 1, 4]))
+
+// Update Inventory -------------------------------------------------------------
+// function updateInventory(arr1, arr2) {
+//     const inventoryMap = new Map(arr1.map(item => [item[1], item[0]]))
+
+//     for (const [quantity, item] of arr2) {
+//         if(inventoryMap.has(item)) {
+//             inventoryMap.set(item, inventoryMap.get(item) + quantity)
+//         } else {
+//             inventoryMap.set(item, quantity)
+//         }
+//     }
+//     return Array.from(inventoryMap, ([item, quantity]) => [quantity, item])
+//         .sort((a, b) => a[1].localeCompare(b[1]))
+// }
+
+console.log(updateInventory([[21, "Bowling Ball"], [2, "Dirty Sock"], [1, "Hair Pin"], [5, "Microphone"]], [[2, "Hair Pin"], [3, "Half-Eaten Apple"], [67, "Bowling Ball"], [7, "Toothpaste"]]))
