6a9ebd191dffeba9adf3ceedd4ad4ad07e3df13b
Michael Harrington <mdharr7@gmail.com>
2023-12-27 18:37:06 +0800

    Write quick sort algorithm in JavaScript.

--- exercises/algorithms/script.js
+++ exercises/algorithms/script.js
@@ -579,4 +579,24 @@
 // Quick Sort -------------------------------------
 // Problem - Given an array of integers, sort the array
 
-// const arr = [-6, 20, 8, -2, 4]
+const nums = [-6, 20, 8, -2, 4]
+
+function quickSort(arr) {
+    if(arr.length <= 1) {
+        return arr
+    }
+
+    const pivot = arr[arr.length-1]
+    const left = []
+    const right = []
+    for(let i = 0; i < arr.length-1; i++) {
+        if(arr[i] < pivot) {
+            left.push(arr[i])
+        } else {
+            right.push(arr[i])
+        }
+    }
+    return [...quickSort(left), pivot, ...quickSort(right)]
+}
+
+console.log(quickSort(nums))
