777ad5b24f2ff0bde96d26630f189aa1d1ba11e3
Michael Harrington <mdharr7@gmail.com>
2023-12-27 16:40:30 +0800

    Write solution for retrieving all possible permutations of a string using recursion, then returning the number of permutations which do not contain matching consecutive letters.

--- exercises/free-code-camp-interview-prep/script.js
+++ exercises/free-code-camp-interview-prep/script.js
@@ -49,3 +49,73 @@
 // }
 
 // console.log(updateInventory([[21, "Bowling Ball"], [2, "Dirty Sock"], [1, "Hair Pin"], [5, "Microphone"]], [[2, "Hair Pin"], [3, "Half-Eaten Apple"], [67, "Bowling Ball"], [7, "Toothpaste"]]))
+
+// function permute(str) {
+//     if (str.length <= 1) {
+//         return [str];
+//     }
+
+//     let permutations = [];
+//     for (let i = 0; i < str.length; i++) {
+//         let char = str[i];
+//         let remainingChars = str.slice(0, i) + str.slice(i + 1);
+//         for (let permutation of permute(remainingChars)) {
+//             permutations.push(char + permutation);
+//         }
+//     }
+//     return permutations;
+// }
+
+// function noConsecutiveRepeats(str) {
+//     return permute(str).filter(perm => {
+//         for (let i = 0; i < perm.length - 1; i++) {
+//             if (perm[i] === perm[i + 1]) {
+//                 return false;
+//             }
+//         }
+//         return true;
+//     }).length;
+// }
+
+// console.log(noConsecutiveRepeats("aab"));
+
+
+
+function permute(str) {
+    if (str.length <= 1) {
+        return [str]
+    }
+
+    const permutations = []
+    for (let i = 0; i < str.length; i++) {
+        const char = str[i]
+        const remainingChars = str.slice(0, i) + str.slice(i + 1);
+        for (const permutation of permute(remainingChars)) {
+            permutations.push(char + permutation)
+        }
+    }
+    return permutations
+}
+
+function noConsecutives(arr) {
+    return arr.filter(perm => {
+        for (let i = 0; i < perm.length - 1; i++) {
+            if (perm[i] === perm[i + 1]) {
+                return false
+            }
+        }
+        return true
+    }).length
+}
+
+function permAlone(str) {
+    const allPerms = permute(str)
+    return noConsecutives(allPerms)
+}
+
+// Test cases
+console.log(permAlone("aab"))
+console.log(permAlone("aaa"))
+console.log(permAlone("aabb"))
+
+
