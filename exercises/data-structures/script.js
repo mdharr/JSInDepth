c99cfcacf71758198274ff3e672e5a70723e9837
Michael Harrington <mdharr7@gmail.com>
2023-12-30 10:45:15 +0800

    Optimized Queue implementation using JavaScript object.

--- exercises/data-structures/script.js
+++ exercises/data-structures/script.js
@@ -494,41 +494,90 @@
 
 */
 
+// class Queue {
+//     constructor() {
+//         this.items = []
+//     }
+//     enqueue(element) {
+//         this.items.push(element)
+//     }
+//     dequeue() {
+//         return this.items.shift()
+//     }
+//     isEmpty() {
+//         return this.items.length === 0
+//     }
+//     peek() {
+//         if(!this.isEmpty()) {
+//             return this.items[0]
+//         }
+//         return null
+//     }
+//     size() {
+//         return this.items.length
+//     }
+//     print() {
+//         console.log(this.items.toString())
+//     }
+// }
+
+// const myQueue = new Queue()
+// console.log(myQueue.isEmpty())
+// myQueue.enqueue(10)
+// myQueue.enqueue(20)
+// myQueue.enqueue(30)
+// console.log(myQueue.size())
+// myQueue.print()
+// console.log(myQueue.dequeue())
+// console.log(myQueue.peek())
+
+// Queue Data Structure Optimized
+
 class Queue {
     constructor() {
-        this.items = []
+        this.items = {}
+        this.rear = 0
+        this.front = 0
     }
+
     enqueue(element) {
-        this.items.push(element)
+        this.items[this.rear] = element
+        this.rear++
     }
+
     dequeue() {
-        return this.items.shift()
+        const item = this.items[this.front]
+        delete this.items[this.front]
+        this.front++
+        return item
     }
+
     isEmpty() {
-        return this.items.length === 0
+        return this.rear - this.front === 0
     }
+
     peek() {
-        if(!this.isEmpty()) {
-            return this.items[0]
-        }
-        return null
+        return this.items[this.front]
     }
+
     size() {
-        return this.items.length
+        return this.rear - this.front
     }
+
     print() {
-        console.log(this.items.toString())
+        console.log(this.items)
     }
 }
 
-const myQueue = new Queue()
-console.log(myQueue.isEmpty())
-myQueue.enqueue(10)
-myQueue.enqueue(20)
-myQueue.enqueue(30)
-console.log(myQueue.size())
-myQueue.print()
-console.log(myQueue.dequeue())
-console.log(myQueue.peek())
+const queue = new Queue()
+console.log(queue.isEmpty())
+queue.enqueue(10)
+queue.enqueue(20)
+queue.enqueue(30)
+console.log(queue.size())
+queue.print()
+console.log(queue.dequeue())
+console.log(queue.peek())
 
-
+// enqueue and dequeue have constant big-o time complexity
+// O(1)
