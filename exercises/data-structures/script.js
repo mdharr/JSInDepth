ee78a4f325261b31805543948746c5646f61ea0e
Michael Harrington <mdharr7@gmail.com>
2023-12-30 08:24:16 +0800

    Notes on Queues.

--- exercises/data-structures/script.js
+++ exercises/data-structures/script.js
@@ -445,5 +445,35 @@
 
 // Queue ------------------------------------
 
+/* 
+    The queue data structure is a sequential collection
+    of elements that follows the principle of First In
+    First Out (FIFO).
+
+    The first element inserted into the queue is first
+    element to be removed.
+
+    A queue of people. People enter the queue at one end
+    (rear/tail) and leave the queue from the other end
+    (front/head).
+
+    Queue is an abstract data type. It is defined by its
+    behavior rather than being a mathematical model.
+
+    The queue data structure supports two main operations.
+        - Enqueue, which adds an element to the rear/tail
+        - Dequeue, which removes an element from the front/head
+*/
+
+// Queue Usage
+
+/* 
+    Printers (printing multiple documents)
+
+    CPU task scheduling (self-explanatory)
+
+    Callback queue in JavaScript runtime (determine order of code execution)
+
+*/
 
 
