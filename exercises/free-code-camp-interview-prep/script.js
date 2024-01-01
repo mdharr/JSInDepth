e1bd955dd665d8d79cb74183b22d8c38b741c045
Michael Harrington <mdharr7@gmail.com>
2024-01-01 10:27:20 +0800

    Function for determing vote results by age group

--- exercises/free-code-camp-interview-prep/script.js
+++ exercises/free-code-camp-interview-prep/script.js
@@ -435,14 +435,46 @@
 //  console.log(shoppingSpree(wishlist));
 
 
-function flatten(arr) {
-    return arr.flat()   
- }
+// function flatten(arr) {
+//     return arr.flat()   
+//  }
  
- const arrays = [
-     ["1", "2", "3"],
-     [true],
-     [4, 5, 6]
- ];
+//  const arrays = [
+//      ["1", "2", "3"],
+//      [true],
+//      [4, 5, 6]
+//  ];
  
- console.log(flatten(arrays));
+//  console.log(flatten(arrays));
+
+
+
+var voters = [
+    {name:'Bob' , age: 30, voted: true},
+    {name:'Jake' , age: 32, voted: true},
+    {name:'Kate' , age: 25, voted: false},
+    {name:'Sam' , age: 20, voted: false},
+    {name:'Phil' , age: 21, voted: true},
+    {name:'Ed' , age:55, voted:true},
+    {name:'Tami' , age: 54, voted:true},
+    {name: 'Mary', age: 31, voted: false},
+    {name: 'Becky', age: 43, voted: false},
+    {name: 'Joey', age: 41, voted: true},
+    {name: 'Jeff', age: 30, voted: true},
+    {name: 'Zack', age: 19, voted: false}
+];
+
+function voterResults(arr) {
+   // your code here
+}
+
+console.log(voterResults(voters)); // Returned value shown below:
+/*
+{ numYoungVotes: 1,
+  numYoungPeople: 4,
+  numMidVotesPeople: 3,
+  numMidsPeople: 4,
+  numOldVotesPeople: 3,
+  numOldsPeople: 4 
+}
+*/
