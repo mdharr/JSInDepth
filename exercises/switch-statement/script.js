7c8e7c987ee86aca61be2f8de59275e3f5101bc3
Michael Harrington <mdharr7@gmail.com>
2024-01-06 15:28:22 +0800

    .

--- exercises/switch-statement/script.js
+++ exercises/switch-statement/script.js
@@ -141,25 +141,25 @@
 */
 
 
-const points = [3, 4, 1, 0, 1, 5, 6]
+// const points = [3, 4, 1, 0, 1, 5, 6]
 
-for(let i = 0; i < points.length; i++) {
-    switch(points[i]) {
-        case 0:
-        case 1:
-        case 2: {
-            console.log("Good try!")
-            break
-        }
-        case 3:
-        case 4:
-        case 5: {
-            console.log("Very well done!")
-            break
-        }
-        default: {
-            console.log("Wow, shooting for the moon!")
-            break
-        }
-    }
-}
+// for(let i = 0; i < points.length; i++) {
+//     switch(points[i]) {
+//         case 0:
+//         case 1:
+//         case 2: {
+//             console.log("Good try!")
+//             break
+//         }
+//         case 3:
+//         case 4:
+//         case 5: {
+//             console.log("Very well done!")
+//             break
+//         }
+//         default: {
+//             console.log("Wow, shooting for the moon!")
+//             break
+//         }
+//     }
+// }
