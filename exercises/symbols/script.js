bd8e1c244e0535ff020d0b6bb294abab871d5790
Michael Harrington <mdharr7@gmail.com>
2024-01-10 14:50:53 +0800

    Compare descriptions of two symbols

--- exercises/symbols/script.js
+++ exercises/symbols/script.js
@@ -18,6 +18,10 @@ const gorilla = Symbol("Monkey")
 
 console.log(typeof baboon)
 console.log(typeof gorilla)
+
+console.log(baboon.description)
+console.log(gorilla.description)
+
 console.log(baboon === gorilla)
 
 
