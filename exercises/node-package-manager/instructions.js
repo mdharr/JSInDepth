48e4f161a42daf3a0e9c6f5876a955c1ff1e72e7
Michael Harrington <mdharr7@gmail.com>
2024-01-11 16:31:16 +0800

    Complete first exercise in node package manager section

---
+++ exercises/node-package-manager/instructions.js
@@ -0,0 +1,93 @@
+/*
+    1. Create a folder for this exercise called 'warmup'
+    2. Inside 'warmup' run 'npm init' to create a default
+       package.json file (press enter to skip answers)
+    3. Add "type": "module" to the package.json so we can
+       use es modules (import/export)
+
+    4. Use NPM to install the "lodash-es" package
+    5. Create a file called "main.js" and import "camelCase"
+       from lodash-es
+    6. Use the function on the String "I love Javascript!" to
+       test if it works and print it out
+    7. Use node to run "main.js"
+
+    *BONUS: Find the camelCase function/file inside of
+            node_modules and read through it
+*/
+
+
+
+
+
+
+/*
+    1. Create a folder for this exercise called 'exercise1'
+    2. Inside 'exercise1' run 'npm init' to create a default
+       package.json file (press enter to skip answers)
+    3. Add "type": "module" to the package.json so we can
+       use es modules (import/export)
+
+    4. Use NPM to install the "chalk" package
+    5. Create a file called "main.js" and import chalk
+       (refer to the npm page or the docs as to how)
+    6. Use chalk to print out some Strings in different colours
+       like blue and red.
+    7. Use node to run "main.js"
+
+    *BONUS: What dependencies does chalk rely on?
+            Where would you find this info ^?
+*/
+
+
+
+
+
+
+
+
+/*
+    1. Create a folder for this exercise called 'exercise2'
+    2. Inside 'exercise2' run 'npm init' to create a default
+       package.json file (press enter to skip answers)
+
+    3. Use NPM to install the "fs-extra" package
+    4. Create a file called "main.js" and import fs-extra
+       (refer to the npm page or the docs as to how)
+       NOTE: This package uses the CommonJS syntax, NOT ES Modules
+    6. Use fs-extra to create a text file in the current directory
+       with the text "Hello, from NodeJS!" "hello.txt"
+    7. Use node to run "main.js"
+
+    *BONUS: What are the other functions fs-extra has that might
+            be useful?
+*/
+
+
+
+
+
+
+
+
+/*
+    1. Create a folder for this exercise called 'exercise3'
+    2. Inside 'exercise3' run 'npm init' to create a default
+       package.json file (press enter to skip answers)
+    3. Add "type": "module" to the package.json so we can
+       use es modules (import/export)
+
+    4. Use NPM to install the "commander" package
+    5. Create a file called "hello.js" and import { program }
+       from commander (refer to the npm page or the docs as to how)
+    6. Write code to allow us to run: node hello.js -n monkey
+       Which will output: Hello, monkey!
+       (-n will be a flag for the "name" argument which can be any
+        single user-provided word)
+
+    *This is challenging and requires a lot of trial and error and back
+     and forth on NPM's website and this package's examples/docs 
+*/
+
+
+
