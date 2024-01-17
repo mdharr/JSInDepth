bfd7ef32211c3936731680b41056752d1a30c968
Michael Harrington <mdharr7@gmail.com>
2024-01-17 14:20:58 +0800

    Write recursive solution for summing an array of numbers

---
+++ exercises/recursion/nums.js
@@ -0,0 +1,16 @@
+const nums = [1, 2, 3, 4, 5, 6]
+
+const sum = (array, index) => {
+    if(index === array.length - 1) {
+        console.log(`BASE CASE: Index ${index}`)
+        return array[index]
+    }
+    console.log(`At index: ${index}`)
+    const total = array[index] + sum(array, index + 1)
+    console.log(`Total at index: ${index} is ${total}`)
+    return total 
+}
+
+const result = sum(nums, 0)
+
+console.log(result)
