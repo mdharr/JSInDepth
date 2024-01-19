fe1cafb5c557332a3c8249899c97d3fd8924c9bf
Michael Harrington <mdharr7@gmail.com>
2024-01-19 14:33:40 +0800

    Complete first exercise in prototypes and prototypal inheritance

---
+++ exercises/prototypes-and-prototypal-inheritance/exercises.js
@@ -0,0 +1,107 @@
+/*
+    1. Add a new function called "boop" to the prototype
+       of String. This function should simply log out the
+       String "Boop!".
+
+    2. Create a new String with the value "meow" and
+       assign it to a variable called "cat".
+
+    3. Call the "boop" method on "cat" to make sure it
+       works correctly.
+*/
+
+String.prototype.boop = () => {
+    console.log("Boop!")
+}
+
+const cat = "meow"
+cat.boop()
+
+
+
+
+/*
+    1. Log out all the properties within the Object
+       prototype. (Only it's own properties)
+
+    2. Log out all the properties within the Array
+       prototype. (Only it's own properties)
+
+    3. Log out all the properties within the Array
+       prototype's prototype.
+
+    4. Does this make sense? Why?
+*/
+
+
+
+
+
+/*
+    1. Create a new Object and assign it to a variable
+       called "fakeArray". Give it the following value:
+       { "0": "Zero", "1": "One", "2": "Two", length: 3}
+
+    2. Set the prototype of "fakeArray" to be Array.prototype
+
+    3. Use the map method on "fakeArray" to log out each item
+       in the array.
+
+    4. What is happening here and why does this work?
+*/
+
+
+
+
+
+
+/*
+    1. Create a new Object and assign it to a variable
+       called "store". Give it the following value:
+       { name: null, stock: [
+        { name: 'candy', quantity: 100 },
+        { name: 'fruit', quantity: 7 },
+        { name: 'toys', quantity: 23 }] }
+
+    2. Create another new Object called "myStore" and assign
+       it to an empty Object. Then, set it's name to:
+       "My Wonderful Store"
+    3. Set the prototype of "myStore" to be "store"
+
+    4. Create another new Object called "yourStore" and assign
+       it to an empty  Object. Then, set it's name to:
+       "Not My Wonderful Store"
+    5. Set the prototype of "yourStore" to be "store"
+
+    6. Modify the quantity of fruit in "myStore" to be 300
+    7. Log out the name and stock from "myStore"
+    8. Log out the name and stock from "yourStore"
+
+    9. What is happening and Why?
+*/
+
+
+
+
+
+
+/*
+    1. Create the following  function:
+
+       function Person(name) {
+         this.name = name;
+       }
+    
+    2. Create an instance of person in 2 ways:
+       const cleo = Person("Cleo")
+       const taylor = new Person("Taylor")
+
+    3. Log out the own property names of each of these
+       variables. What happens?
+
+    4. Log out just taylor's own property names. Does this
+       work? Why?
+
+    5. Log out the prototype of cleo and taylor. What is
+       happening? What about just taylor?
+*/
