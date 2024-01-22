b584f3370fb017a01028869fbdf7ad039a4e00de
Michael Harrington <mdharr7@gmail.com>
2024-01-22 18:23:29 +0800

    Complete exercise 3 in encapsulation and static keyword practice section

--- exercises/encapsulation-and-static-keyword/exercises/exercise-5.js
+++ exercises/encapsulation-and-static-keyword/exercises/exercise-5.js
@@ -7,3 +7,18 @@
     2. How about private or public accessors for these
        static or instance fields/methods/properties?
 */
+
+/*
+    If I were to design my own class for a project, when desciding
+    to make something a static or instance field/method/property,
+    I would ask the following:
+    - Is it necessary to instantiate a new object to use this method?
+    - Will the functionality be unique to each class instance?
+    - How often will I need to use these fields/methods/properties?
+
+    For private or public accessors, I would ask:
+    - Should the user/developer have direct access to them?
+    - Will unlimited access to these fields/methods/properties
+    potentially cause issues and crash the program by leading
+    to unintended use or unexpected bugs
+*/
