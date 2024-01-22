2f76f9e065f226ec33f3943bec576d82e1d4f051
Michael Harrington <mdharr7@gmail.com>
2024-01-22 11:30:09 +0800

    Complete exercise 4 in encapsulation and polymorphism

--- exercises/encapsulation-and-static-keyword/exercises/exercise-4.js
+++ exercises/encapsulation-and-static-keyword/exercises/exercise-4.js
@@ -35,3 +35,73 @@
        // A red rectangle
        // A round and awesome green circle
 */
+
+
+class Shape {
+   type = "Shape"
+
+   constructor(color = "Transparent") {
+      this.color = color
+   }
+
+   describe() {
+      console.log(`A ${this.color} ${this.type}`)
+   }
+}
+
+class Square extends Shape {
+   type = "Square"
+
+   constructor(color, sideLength) {
+      super(color)
+      this.sideLength = sideLength
+   }
+
+   area() {
+      return this.sideLength**2
+   }
+}
+
+class Rectangle extends Shape {
+   type = "Rectangle"
+
+   constructor(color, width, height) {
+      super(color)
+      this.width = width
+      this.height = height
+   }
+
+   area() {
+      return this.width * this.height
+   }
+}
+
+class Circle extends Shape {
+   type = "Circle"
+
+   constructor(color, radius) {
+      super(color)
+      this.radius = radius
+   }
+
+   area() {
+      return ((this.radius**2) * Math.PI).toFixed(5)
+   }
+
+   describe() {
+      console.log(`A round and awesome ${this.color} ${this.type}`)
+   }
+}
+
+const square = new Square("blue", 5)
+const rectangle = new Rectangle("red", 5, 6)
+const circle = new Circle("green", 3)
+console.log( square.area() ) // 25
+console.log( rectangle.area() ) // 30
+console.log( circle.area() ) // 28.27433
+for ( const shape of [square, rectangle, circle] ) {
+ shape.describe()
+}
+// A blue square
+// A red rectangle
+// A round and awesome green circle
