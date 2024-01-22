414186117740e2c0ed2039ccd4f1db328109a541
Michael Harrington <mdharr7@gmail.com>
2024-01-22 18:33:28 +0800

    Complete exercise 4 in encapsulation and static keyword practice section

--- exercises/encapsulation-and-static-keyword/exercises/exercise-3.js
+++ exercises/encapsulation-and-static-keyword/exercises/exercise-3.js
@@ -33,3 +33,37 @@
        console.log(Object.getOwnPropertyNames(Monkey))
        console.log(Object.getOwnPropertyNames(Monkey.__proto__))
 */
+
+class Animal {
+    static knownMammals = []
+    mammal = false
+    eyes = 2
+    // constructor(mammal, eyes) {
+    //     this.mammal = mammal
+    //     this.eyes = eyes
+    // }
+
+    static isMammal() {}
+    describe() {}
+}
+
+class Monkey extends Animal {
+    static knownMonkeys = []
+    height
+    weight
+
+    static isCute() {}
+    eatBanana() {}
+}
+
+   const animal = new Animal(false, 2)
+   console.log(Object.getOwnPropertyNames(animal)) // non static fields in or outside of the constructor
+   console.log(Object.getOwnPropertyNames(animal.__proto__)) // constructor and other non static methods
+   console.log(Object.getOwnPropertyNames(Animal)) // static methods/properties and prototype
+   console.log(Object.getOwnPropertyNames(Animal.__proto__)) // prototype properties/methods? (in this case the prototype is Function)
+
+   const monkey = new Monkey()
+   console.log(Object.getOwnPropertyNames(monkey)) // inherited non static fields and own fields in or outside of constructor
+   console.log(Object.getOwnPropertyNames(monkey.__proto__)) // constructor and non static methods
+   console.log(Object.getOwnPropertyNames(Monkey)) // static methods/properties and prototype
+   console.log(Object.getOwnPropertyNames(Monkey.__proto__)) // prototype properties/methods? (in this case the prototype is Animal)
