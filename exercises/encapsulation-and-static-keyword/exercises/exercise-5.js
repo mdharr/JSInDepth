7a4ad10b2dd58f032791a993c143c6860a5bfa1e
Michael Harrington <mdharr7@gmail.com>
2024-01-22 11:45:31 +0800

    Complete exercise 5 in encapsulation and polymorphism

--- exercises/encapsulation-and-static-keyword/exercises/exercise-5.js
+++ exercises/encapsulation-and-static-keyword/exercises/exercise-5.js
@@ -17,3 +17,120 @@
     * This is purposefully open-ended to allow you
       the creative freedom to design these classes
 */
+
+class Entity {
+   type = "Entity"
+
+   constructor(name, hp, speed, yPos, xPos, sprite) {
+      this.name = name
+      this.hp = hp
+      this.speed = speed
+      this.xPos = xPos
+      this.yPos = yPos
+      this.sprite = sprite
+   }
+
+   move() {
+
+   }
+
+   shoot() {
+
+   }
+}
+
+class Player extends Entity {
+   type = "Player"
+
+   constructor(name, hp, speed, yPos, xPos, sprite) {
+      super(name, hp, speed, yPos, xPos, sprite)
+   }
+
+   move() {
+
+   }
+
+   shoot() {
+
+   }
+}
+
+class Enemy extends Entity {
+   type = "Enemy"
+   constructor(name, hp, speed, yPos, xPos, sprite) {
+      super(name, hp, speed, yPos, xPos, sprite)
+   }
+
+   move() {
+
+   }
+
+   shoot() {
+
+   }
+}
+
+class LargeInvader extends Enemy {
+   constructor(name, hp, speed, yPos, xPos, sprite) {
+      super(name, hp, speed, yPos, xPos, sprite)
+   }
+
+   move() {
+      // slowest movement speed
+   }
+
+   shoot() {
+      // highest damage shot
+   }
+}
+
+class MediumInvader extends Enemy {
+   constructor(name, hp, speed, yPos, xPos, sprite) {
+      super(name, hp, speed, yPos, xPos, sprite)
+   }
+
+   move() {
+      // medium movement speed
+   }
+
+   shoot() {
+      // medium damage shot
+   }
+}
+
+class SmallInvader extends Enemy {
+   constructor(name, hp, speed, yPos, xPos, sprite) {
+      super(name, hp, speed, yPos, xPos, sprite)
+   }
+
+   move() {
+      // fastest movement speed
+   }
+
+   shoot() {
+      // lowest damage shot
+   }
+}
+
+class UFO extends Enemy {
+   constructor(name, hp, speed, yPos, xPos, sprite) {
+      super(name, hp, speed, yPos, xPos, sprite)
+   }
+
+   move() {
+      // high movement speed
+   }
+
+   shoot() {
+      // high damage shot
+   }
+
+   chargeUp() {
+      // charge attack in preparation to use lasers
+      // cancels charge if enough damage dealt
+   }
+
+   laserAttack() {
+      // shoots lasers as special attack
+   }
+}
